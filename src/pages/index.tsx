abb4e0c12dbc6d2d93d779f02ab37265757aef84
jun094 <jun094@naver.com>
2022-07-29 00:08:11 +0900

    feat: tawilwind, daisUI 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 
-function index() {
-    return <div>HI :)</div>;
+function IndexPage() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">123123</h1>
+            <button className="btn btn-primary">Button</button>
+        </>
+    );
 }
 
-export default index;
+export default IndexPage;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    content: [
+        "./src/pages/**/*.{js,jsx,ts,tsx}",
+        "./src/components/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    // 적용 완료
+                    primary: "#9DD2FE",
+
+                    // 적용 전
+                    secondary: "#D926A9",
+                    accent: "#1FB2A6",
+                    neutral: "#191D24",
+                    "base-100": "#2A303C",
+                    info: "#3ABFF8",
+                    success: "#36D399",
+                    warning: "#FBBD23",
+                    error: "#F87272",
+                },
+            },
+        ],
+    },
+};
