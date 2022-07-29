32f41e3378c1ee813135c95bf604322f28a4795a
jun094 <jun094@naver.com>
2022-07-29 22:51:51 +0900

    env: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 module.exports = {
     content: [
-        "./src/pages/**/*.{js,jsx,ts,tsx}",
-        "./src/components/**/*.{js,jsx,ts,tsx}",
+        './src/pages/**/*.{js,jsx,ts,tsx}',
+        './src/components/**/*.{js,jsx,ts,tsx}',
     ],
     theme: {
         extend: {},
     },
-    plugins: [require("daisyui")],
+    plugins: [require('daisyui')],
     daisyui: {
         themes: [
             {
                 holymolyTheme: {
                     // 적용 완료
-                    primary: "#9DD2FE",
+                    primary: '#9DD2FE',
+                    'base-100': '#FFF',
 
                     // 적용 전
-                    secondary: "#D926A9",
-                    accent: "#1FB2A6",
-                    neutral: "#191D24",
-                    "base-100": "white",
-                    info: "#3ABFF8",
-                    success: "#36D399",
-                    warning: "#FBBD23",
-                    error: "#F87272",
+                    secondary: '#D926A9',
+                    accent: '#1FB2A6',
+                    neutral: '#191D24',
+                    info: '#3ABFF8',
+                    success: '#36D399',
+                    warning: '#FBBD23',
+                    error: '#F87272',
                 },
             },
         ],
     },
-};
+}
