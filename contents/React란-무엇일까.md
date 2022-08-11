330531a4f16e5af235e3ecd4fa636c108d6c5fa6
jun094 <jun094@naver.com>
2022-08-12 00:13:51 +0900

    feat: slug 생성

---
+++ contents/React란-무엇일까.md
@@ -0,0 +1,20 @@
+---
+date: '2022-08-11'
+title: 'Test'
+summary: 'BlaBlaBlaBlaBlaBlaBlaBla'
+thumbnail: './test.png'
+---
+
+### 1. Bla Bla Bla Bla
+
+BlaBlaBlaBlaBlaBlaBlaBla
+
+BlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBla
+
+### 2. Bla Bla Bla Bla Bla Bla Bla Bla
+
+BlaBlaBlaBlaBlaBlaBlaBla
+
+BlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBla
+
+---
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,9 @@
 // Webpack Config
 
 const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
+//*** webpack 절대 경로 설정
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
     const output = getConfig().output || {}
 
@@ -14,4 +16,15 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
             },
         },
     })
+}
+
+//*** 마크다운 데이터 Slug 생성
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions
+
+    if (node.internal.type === `MarkdownRemark`) {
+        const slug = createFilePath({ node, getNode })
+
+        createNodeField({ node, name: 'slug', value: slug })
+    }
 }
